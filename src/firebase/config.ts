17076886522118f33954e78e624beb0fcbd57ae8
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-06 23:05:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1563376469-8d917",
+  "appId": "1:1060362955120:web:ae6bebf16d41fd40f747b5",
+  "apiKey": "AIzaSyCGc3sD1-p7sGThjVSkqOZbCD2ZYNvkOUE",
+  "authDomain": "studio-1563376469-8d917.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1060362955120"
+};
