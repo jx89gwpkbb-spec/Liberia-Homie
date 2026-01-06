c617653b1e460738647650fe949aed510b6bda9f
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-06 23:01:33 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <div className="flex min-h-screen flex-col">
           <Header />
           <main className="flex-1">{children}</main>
