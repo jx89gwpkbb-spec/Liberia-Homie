d528ddb36a1af5c93c02ca7b390c53eaf11ad421
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 03:57:14 +0000

    built Localization & Multi-language Support

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => {
   // Provide a static path to the messages directory
-  const messages = (await import(`../messages/${locale}.json`)).default;
+  const messages = (await import(`./messages/${locale}.json`)).default;
  
   return {
     messages
