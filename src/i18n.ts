d5d0f98503201998c28fef29b8b8178c68bca6a2
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 20:47:17 +0000

    Try fixing this error: `Console Error: Unable to find `next-intl` locale

---
+++ src/i18n.ts
@@ -0,0 +1,14 @@
+import {notFound} from 'next-intl';
+import {getRequestConfig} from 'next-intl/server';
+ 
+// A list of all locales that are supported
+const locales = ['en', 'es'];
+
+export default getRequestConfig(async ({locale}) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
+ 
+  return {
+    messages: (await import(`./messages/${locale}.json`)).default
+  };
+});
