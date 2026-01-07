cc9505c4ea04263eeb4a2f28cdf3030e249d56bd
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 01:12:24 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1563376469-8d917",
   "appId": "1:1060362955120:web:ae6bebf16d41fd40f747b5",
+  "storageBucket": "studio-1563376469-8d917.appspot.com",
   "apiKey": "AIzaSyCGc3sD1-p7sGThjVSkqOZbCD2ZYNvkOUE",
   "authDomain": "studio-1563376469-8d917.firebaseapp.com",
   "measurementId": "",
