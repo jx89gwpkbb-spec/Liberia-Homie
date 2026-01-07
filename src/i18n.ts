beaaaa7d3f509c21dd2b44640a7110e447842f82
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 03:56:06 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,19 @@
 import {getRequestConfig} from 'next-intl/server';
  
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./messages/${locale}.json`)).default
-}));
+export default getRequestConfig(async ({locale}) => {
+  let messages;
+  switch (locale) {
+    case 'en':
+      messages = (await import('../messages/en.json')).default;
+      break;
+    case 'es':
+      messages = (await import('../messages/es.json')).default;
+      break;
+    default:
+      messages = (await import('../messages/en.json')).default;
+  }
+ 
+  return {
+    messages
+  };
+});
