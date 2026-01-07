d43bdcf6d368f2a005c43818255a52c93ba66311
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 03:56:33 +0000

    Try fixing this error: `Runtime Error: ./src/i18n.ts:4:20 Module not fou

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => {
-  let messages;
-  switch (locale) {
-    case 'en':
-      messages = (await import('../messages/en.json')).default;
-      break;
-    case 'es':
-      messages = (await import('../messages/es.json')).default;
-      break;
-    default:
-      messages = (await import('../messages/en.json')).default;
-  }
+  // Provide a static path to the messages directory
+  const messages = (await import(`../messages/${locale}.json`)).default;
  
   return {
     messages
