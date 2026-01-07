c14a7a9e170356c21ba1e460608e0de950797a95
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 03:44:50 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 import withNextIntl from 'next-intl/plugin';
 
-const withIntl = withNextIntl('./src/i18n.ts');
+const withIntl = withNextIntl();
 
 const nextConfig: NextConfig = {
   /* config options here */
