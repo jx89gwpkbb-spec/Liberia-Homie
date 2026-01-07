ce615963d4460cdda8833faf5d4e019f54c0b776
Samuel Nimely <samuelknimelyjr@gmail.com>
2026-01-07 03:45:48 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 import withNextIntl from 'next-intl/plugin';
 
-const withIntl = withNextIntl();
+const withIntl = withNextIntl('./next-intl.config.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
